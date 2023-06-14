8a07c32090c1379121413e5c698e37387e9a8c78
Sagor Ahamed <sagorahamed251245@gmail.com>
2023-06-14 21:19:35 +0600

    modify

--- index.js
+++ index.js
@@ -27,13 +27,19 @@ app.get('/', (req, res) => {
 const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASSWORD}@cluster0.ry6i5bk.mongodb.net/?retryWrites=true&w=majority`;
 
+
+
+
 // Create a MongoClient with a MongoClientOptions object to set the Stable API version
 const client = new MongoClient(uri, {
     serverApi: {
         version: ServerApiVersion.v1,
         strict: true,
         deprecationErrors: true,
-    }
+    },
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+    maxPoolSize: 10,
 });
 
 // verifyJwt
@@ -60,7 +66,12 @@ const verifyJWT = (req, res, next) => {
 async function run() {
     try {
         // Connect the client to the server	(optional starting in v4.7)
-        await client.connect();
+        client.connect((err) => {
+            if (err) {
+                console.log(err)
+                return;
+            }
+        });
 
         // Send a ping to confirm a successful connection
 
